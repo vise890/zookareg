2935d42c48e7ec9132cc2bfa9588ef16e89fb280
Martino Visintin <vise890@gmail.com>
2017-08-18 20:19:47 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vise890/zookareg "0.1.0-SNAPSHOT"
+(defproject vise890/zookareg "0.1.0"
   :description "Embedded `Zo`okeeper `Ka`fka and Confluent's Schema `Reg`istry"
   :url "http://github.com/vise890/zookareg"
   :license {:name "Eclipse Public License"
